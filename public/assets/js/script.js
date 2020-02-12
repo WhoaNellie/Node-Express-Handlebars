e4ca37b4687a9a3c091f4b09f44e3c908b1e93b6
Nellie <fabybacenelson@gmail.com>
2020-02-12 15:16:27 -0700

    added event listeners

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,23 @@
-// event listeners to make api reqests
+$(document).ready(function(){
+
+    // create a new burger
+    $("#newBurger").click(function(){
+        // event.preventDefault();
+        $.ajax("/api/burger", {
+            type: "POST",
+            data: $("#burgerName").val().trim()
+        }).then(function(){
+            location.reload();
+        });
+    });
+
+    // devour a burger
+    $(".devour").click(function(){
+        // event.preventDefault();
+        $.ajax(`/api/burger/${this.data-id}`, {
+            type: "PUT"
+        }).then(function(){
+            location.reload();
+        });
+    });
+});
